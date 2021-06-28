34db506e8f09617130a76ce75d144a2f77a39c34
tim <tjhickey@brandeis.edu>
2021-06-28 07:43:49 -0400

    added Quiz 3 Solution -- Pomodoros

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import L06 from './L06'
 import CameraDemo0 from './components/CameraDemo0'
 import CounterDemo from './components/CounterDemo'
 import ContextDemo from './components/ContextDemo'
+import Pomodoros from './components/Pomodoros'
 
 /*
   Demo App for CS153 Sum21
@@ -11,6 +12,6 @@ import ContextDemo from './components/ContextDemo'
 */
 export default function App() {
     return (
-          <ContextDemo />
+          <Pomodoros />
 );
 }
